cfbb2eb7ef00e4c1a5355cd9c6e34e62e4d9b349
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-02-11 02:56:05 -0300

    Versão 0.1 - Layout completo com barra de participantes

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function aparecerBarra() {
+    const barra = document.querySelector(".nav-bar");
+    barra.classList.remove("hidden");
+}
+
+function esconderBarra() {
+    const barra = document.querySelector(".nav-bar");
+    barra.classList.add("hidden");
+}
