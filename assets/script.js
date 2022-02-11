15f6890fbf653f0ed30d0616bee9dc8f9141f053
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-02-11 16:37:59 -0300

    Versão 0.2 - Adição do visual das mensagens diferentes

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,16 @@
 function aparecerBarra() {
-    const barra = document.querySelector(".nav-bar");
+    const barra = document.querySelector(".side-bar");
     barra.classList.remove("hidden");
 }
 
 function esconderBarra() {
-    const barra = document.querySelector(".nav-bar");
+    const barra = document.querySelector(".side-bar");
     barra.classList.add("hidden");
+}
+
+const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+promise.then(printarData);
+
+function printarData(resposta){
+    console.log(resposta.data[74]);
 }
