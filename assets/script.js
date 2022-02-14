952bc21800bbeb84d828ff73017ebef24ead18b2
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-02-14 04:15:53 -0300

    Versão 0.3 - Recebimento de mensagens, padronização de mensagens e scroll automático completos

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+const userName = prompt("Diga-me seu lindo nome :)");
+let todasMensagens = [];
 function aparecerBarra() {
     const barra = document.querySelector(".side-bar");
     barra.classList.remove("hidden");
@@ -8,9 +10,63 @@ function esconderBarra() {
     barra.classList.add("hidden");
 }
 
-const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-promise.then(printarData);
+function intervalo(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    promise.then(lerMensagens);
 
-function printarData(resposta){
-    console.log(resposta.data[74]);
+    function colocarMensagemNaTela(mensagem) {
+        console.log(mensagem);
+        const pagina = document.querySelector("main");
+        let comparador = 0;
+        let classeScroll = 0;
+        classeScroll++;
+        
+        for(let j = 0; j < todasMensagens.length; j++){
+            if(mensagem.time === todasMensagens[j].time && mensagem.text === todasMensagens[j].text && mensagem.type === todasMensagens[j].type && mensagem.from === todasMensagens[j].from && mensagem.to === todasMensagens[j].to){
+                comparador++;
+            }
+        }
+
+        if(comparador == 0){
+            if(mensagem.type === 'status'){
+                pagina.innerHTML += `
+                <article class="status article" data-identifier="message">
+                    <time>[${mensagem.time}]</time>
+                    <p>${mensagem.from} ${mensagem.text}</p>
+                </article>
+                `;
+                window.scrollTo(0,document.body.scrollHeight);
+            }
+
+            else if(mensagem.type === 'message'){
+                pagina.innerHTML += `
+                <article class="message article" data-identifier="message">
+                    <time>[${mensagem.time}]</time>
+                    <p>${mensagem.from} para ${mensagem.to}: ${mensagem.text}</p>
+                </article>
+                `;
+                window.scrollTo(0,document.body.scrollHeight);
+            }
+
+            else if(mensagem.type === 'private_message' && (mensagem.to === userName || mensagem.to === "Todos")){
+                pagina.innerHTML += `
+                <article class="private_message article" data-identifier="message">
+                    <time>[${mensagem.time}]</time>
+                    <p>${mensagem.from} para ${mensagem.to}: ${mensagem.text}</p>
+                </article>
+                `;
+                window.scrollTo(0,document.body.scrollHeight);
+            }
+        }
+        comparador = 0;
+    }
+
+    function lerMensagens(resposta){
+        resposta.data.forEach(colocarMensagemNaTela);
+        for(let i = 0; i<resposta.data.length; i++){
+            todasMensagens.push(resposta.data[i]);
+        }
+    }
 }
+
+setInterval(intervalo, 3000);
